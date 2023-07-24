cce0e98a6309afb564902925f797f9e8813ff5ee
Zheeeng <hi@zheeeng.me>
2023-07-24 22:53:46 +0800

    chore: bump version

--- packages/react-device-frameset-example/vite.config.ts
+++ packages/react-device-frameset-example/vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import * as path from 'path'
 import react from '@vitejs/plugin-react'
-import mdx from 'vite-plugin-mdx'
 import pages from 'vite-plugin-react-pages'
 import virtualPlainText from 'vite-plugin-virtual-plain-text'
 
 export default defineConfig({
   plugins: [
     react(),
-    mdx(),
     pages({
       pagesDir: path.join(__dirname, 'pages'),
     }),
